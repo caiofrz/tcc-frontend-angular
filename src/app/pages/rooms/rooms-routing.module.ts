4db1edbadeb9f89682e738460fa7e4bfa26d1587
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-29 21:00:46 -0300

    fix(routes - Rooms): removing routes childrens

--- src/app/pages/rooms/rooms-routing.module.ts
+++ src/app/pages/rooms/rooms-routing.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomsComponent } from './rooms.component';
-import { AtualizarComponent } from '../atualizar/atualizar.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RoomsComponent,
-    children: [
-      {
-        path: 'novo',
-        loadChildren: () =>
-          import('../new-room/new-room.module').then((m) => m.NewRoomModule),
-        data: { breadcrumb: 'Cadastrar Quarto' },
-      },
-      {
-        path: 'atualizar',
-        component: AtualizarComponent,
-        data: { breadcrumb: 'Atualizar Quarto' },
-      },
-    ],
   },
 ];
 
